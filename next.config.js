07034801ba2e7bbba1e75cbf41831813ad9a1422
M. Irfan @mirfan.exe <dev.mirfan@gmail.com>
2024-03-03 14:19:36 +0700

    Minor - fix bug experimental server action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["bytee.cloud", "localhost:3000"],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
